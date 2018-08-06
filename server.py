9056f3d0bf6eafbd4c9a795c3134fe21819a1c1e
Brendan Jones <brendan.jones@procore.com>
2018-08-05 19:41:15 -0700

    db create all in server

--- server.py
+++ server.py
@@ -11,6 +11,7 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/2.db'
 app.secret_key = 'A0Zr98j/3yX R~XHH!jmN]LWX/,?RT'  # We need this
 SCOPES = ["https://www.googleapis.com/auth/calendar", "https://www.googleapis.com/auth/userinfo.profile", "http://procore-activities.herokuapp.com/auth/oauth2callback"]
 db.init_app(app)
+db.create_all()
 app.register_blueprint(api, url_prefix='/api')
 app.register_blueprint(auth, url_prefix='/auth')
 @app.route("/")
